4801d33e92754b3b539588c68128c7c8f5de2ac6
sibber11 <sibber11@gmail.com>
2023-07-27 13:44:27 +0600

    nav links extracted

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+import {Link} from '@inertiajs/vue3';
+import NavLinks from "@/Layouts/NavLinks.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -29,9 +29,7 @@ const showingNavigationDropdown = ref(false);
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
+                                <NavLinks/>
                             </div>
                         </div>
 
---
+++ resources/js/Layouts/NavLinks.vue
@@ -0,0 +1,10 @@
+<template>
+    <NavLink :active="route().current('dashboard')" :href="route('dashboard')">
+        Dashboard
+    </NavLink>
+
+</template>
+<script setup>
+import NavLink from "@/Components/NavLink.vue";
+
+</script>
